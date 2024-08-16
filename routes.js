ffb80cacd8f217315e665136e6bcc838d9f20c9e
Kyamran Rizayev <kyamranr@gmail.com>
2024-08-15 23:09:35 -0500

    finished routes.js

---
+++ routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+let items = require("./fakeDb");
+
+router.get("/items", (req, res) => {
+  res.json(items);
+});
+
+router.post("/items", (req, res) => {
+  const newItem = { name: req.body.name, price: req.body.price };
+  items.push(newItem);
+  res.status(201).json({ added: newItem });
+});
+
+router.get("/items/:name", (req, res) => {
+  const foundItem = items.find((item) => item.name === req.params.name);
+  if (!foundItem) return res.status(404).json({ error: "Item not found" });
+  res.json(foundItem);
+});
+
+router.patch("/items/:name", (req, res) => {
+  const foundItem = items.find((item) => item.name === req.params.name);
+  if (!foundItem) return res.status(404).json({ error: "Item not found" });
+
+  foundItem.name = req.body.name || foundItem.name;
+  foundItem.price = req.body.price || foundItem.price;
+
+  res.json({ updated: foundItem });
+});
+
+router.delete("/items/:name", (req, res) => {
+  const itemIndex = items.findIndex((item) => item.name === req.params.name);
+  if (itemIndex === -1)
+    return res.status(404).json({ error: "Item not found" });
+
+  items.splice(itemIndex, 1);
+  res.json({ message: "Deletedd" });
+});
+
+module.exports = router;
